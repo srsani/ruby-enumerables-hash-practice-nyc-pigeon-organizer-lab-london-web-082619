de02f3563dd50e3fcabf3d3e863434321b947779
droll-accelerator-1483 <srsani@gmail.com>
2019-07-26 17:29:41 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  all_name = []
+  for i,j in pigeon_data
+    for k in j
+      all_name= all_name + (k[1].each{|name| name})
+    end
+  end
+  all_name.uniq()
+
+  keys = []
+  for key in pigeon_data
+    keys = keys.append(key[0])
+  end
+  new_hash = {}
+  for name in all_name
+    new_hash[name] = {color: [], gender: [], lives: []}
+    for key in keys
+      for i, j in pigeon_data[key]
+        if j.include?(name)
+           new_hash[name][key].append(i.to_s)
+        end
+      end
+    end
+  end
+  return new_hash
 end
